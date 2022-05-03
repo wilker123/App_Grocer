42c3623f15b842f90d7f074ca6a214752b083c96
wilker123 <josewilkerlucena123@gmail.com>
2022-05-03 14:41:58 -0300

    Created Containers base in SingInScreen

--- greengrocer/lib/src/auth/sign_in_screen.dart
+++ greengrocer/lib/src/auth/sign_in_screen.dart
@@ -5,6 +5,27 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
